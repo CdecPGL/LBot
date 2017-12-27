22e4dc64e040b91cc7622ce50217cac8392c338a
Cdec <cdecrement@gmail.com>
2017-12-28 01:30:07 +0900

    add random reply

--- bot/reply_generators.py
+++ bot/reply_generators.py
@@ -1,6 +1,13 @@
+import random
 
-
+word_list = open("bot/word_list.txt").read().split("\n")
 
 def generate_help():
     return '''<使い方>
     がんばれ。'''
+
+def generate_random_word():
+    return random.choice(word_list)
+
+def generate_random_reply():
+    return "よく分からないけど、" + generate_random_word()
