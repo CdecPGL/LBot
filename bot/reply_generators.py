2f3e76d7053a1f18b32cbef0461af98911de5417
Cdec <cdecrement@gmail.com>
2017-12-28 21:14:42 +0900

    add-database

--- bot/reply_generators.py
+++ bot/reply_generators.py
@@ -1,13 +1,20 @@
 import random
+from bot.models import Vocabulary
 
 word_list = open("bot/word_list.txt").read().split("\n")
+
 
 def generate_help():
     return '''<使い方>
     がんばれ。'''
 
+
 def generate_random_word():
-    return random.choice(word_list)
+    if Vocabulary.objects.count():
+        return Vocabulary.objects.order_by('?').values_list("word")[0]
+    else:
+        return "何にも分からない……"
+
 
 def generate_random_reply(text):
     return text + "はよく分からないけど、" + generate_random_word() + "。"
--- luftelli_bot/settings.py
+++ luftelli_bot/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["luftelli-bot.herokuapp.com"]
 # Application definition
 
 INSTALLED_APPS = [
+    'bot.apps.BotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
