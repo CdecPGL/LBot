3956cd0bfb269f6a6197a6e37f8bf45c16d2bfa4
Cdec <cdecrement@gmail.com>
2018-01-04 23:04:08 +0900

    check_tasksコマンドのメソッドを分割

--- bot/management/commands/check_tasks.py
+++ bot/management/commands/check_tasks.py
@@ -1,6 +1,7 @@
 '''check_tasksコマンド'''
 
 import datetime
+import sys
 from enum import Enum
 
 from django.core.management.base import BaseCommand
@@ -35,13 +36,21 @@ class TaskCheckType(Enum):
     # 明日の重要タスクの確認
     TommorowImportantTasksCheck = "TommorowImportantTasksCheck"
     # タスクの事前確認とリマインド
-    TasksPreRemindAndCheck = "TasksPreRemindAndCheck"
+    SoonTasksRemindAndCheck = "SoonTasksRemindAndCheck"
 
 
 class Command(BaseCommand):
     '''check_tasksコマンド'''
     # python manage.py help count_entryで表示されるメッセージ
     help = 'タスクの更新と告知を行う'
+
+    def __init__(self):
+        super(Command, self).__init__()
+        self.__handler_map = {
+            TaskCheckType.TommorowTasksRemind: Command.handle_tommorow_tasks_remind,
+            TaskCheckType.TommorowImportantTasksCheck: Command.handle_tommorow_important_tasks_check,
+            TaskCheckType.SoonTasksRemindAndCheck: Command.handle_soon_tasks_remind_and_check,
+        }
 
     def add_arguments(self, parser):
         '''コマンドライン引数を指定。
@@ -51,99 +60,111 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         task_check_type = options["task_check_type"]
+        self.__handler_map[task_check_type]()
 
-        if task_check_type == TaskCheckType.TommorowTasksRemind:
-            # 明日が期限のタスクを通知
-            group_task_map = {}
-            for task in Task.objects.filter(deadline__range=get_tommorow_range()):
-                group = task.group
-                if group.line_group.group_id in group_task_map:
-                    group_task_map[group.line_group.group_id].append(task)
-                else:
-                    group_task_map[group.line_group.group_id] = [task]
+    @staticmethod
+    def handle_tommorow_tasks_remind():
+        '''明日が期限のタスクを通知'''
+        group_task_map = {}
+        for task in Task.objects.filter(deadline__range=get_tommorow_range()):
+            group = task.group
+            if group.line_group.group_id in group_task_map:
+                group_task_map[group.line_group.group_id].append(task)
+            else:
+                group_task_map[group.line_group.group_id] = [task]
 
-            for line_group_id, task_list in group_task_map.items():
-                mess = "こんばんは。明日が期限のタスクは以下のとおりだよ。"
+        for line_group_id, task_list in group_task_map.items():
+            mess = "こんばんは。明日が期限のタスクは以下のとおりだよ。"
+            line.api.push_message(
+                line_group_id, TextSendMessage(text=mess))
+            mess = "\n".join(
+                ["■{}(期限: {})".format(task.name, convert_deadline_to_string(task.deadline)) for task in task_list])
+            line.api.push_message(
+                line_group_id, TextSendMessage(text=mess))
+            mess = "おやすみなさい:D"
+            line.api.push_message(
+                line_group_id, TextSendMessage(text=mess))
+
+    @staticmethod
+    def handle_tommorow_important_tasks_check():
+        '''明日が期限の重要タスクを通知(グループのみ)'''
+        # 明日が期限の重要タスクを取得しグループごとにまとめる
+        group_task_map = {}
+        for task in Task.objects.filter(deadline__range=get_tommorow_range(), importance=TaskImportance.High.name, group__isnull=False):
+            group = task.group
+            if group.line_group.group_id in group_task_map:
+                group_task_map[group.line_group.group_id].append(task)
+            else:
+                group_task_map[group.line_group.group_id] = [task]
+
+        # グループごとに通知
+        for line_group_id, task_list in group_task_map.items():
+            group = line.utilities.get_group_by_line_group_id_from_database(
+                line_group_id)
+            # 確認タスクを追加
+            check_number_count = 1
+            for task in task_list:
+                # すでに登録されていたら飛ばす
+                if not TaskJoinCheckJob.objects.filter(task=task).exists():
+                    # 空いている確認番号を探す
+                    while TaskJoinCheckJob.objects.filter(check_number=check_number_count).exists():
+                        check_number_count += 1
+                    # タスク確認の登録(テストで期限を12時間後にする)
+                    TaskJoinCheckJob.objects.create(
+                        group=task.group, task=task, check_number=check_number_count, deadline=datetime.datetime.now() + datetime.timedelta(hours=12))
+
+            # 確認タスク一覧を作成
+            important_task_check_job_list = TaskJoinCheckJob.objects.filter(
+                group=group, task__importance=TaskImportance.High.name)
+            # 確認タスクがなかったらエラー
+            if not important_task_check_job_list.exists():
+                sys.stderr.write(
+                    "グループ「{}」で存在するべきタスク確認ジョブが存在しません。".format(group.name))
+                continue
+            # 確認番号で並び替え
+            ordered_task_check_job_list = [task_check_job for task_check_job in sorted(
+                important_task_check_job_list.all(), key=lambda task_check_job: task_check_job.check_number)]
+
+            # 通知
+            if len(ordered_task_check_job_list) == 1:
+                task_check_job = ordered_task_check_job_list[0]
+                task = task_check_job.task
+                mess = "こんにちは。\n重要なタスク「{}」が明日の{}からあるよ。".format(
+                    task.name, convert_deadline_to_string(task.deadline))
                 line.api.push_message(
                     line_group_id, TextSendMessage(text=mess))
-                mess = "\n".join(
-                    ["■{}(期限: {})".format(task.name, convert_deadline_to_string(task.deadline)) for task in task_list])
+                mess = "メンバーの{}はこのタスクに参加できる？".format(
+                    "".join(["「{}」".format(member.name) for member in task.participants.all()]))
                 line.api.push_message(
                     line_group_id, TextSendMessage(text=mess))
-                mess = "おやすみなさい:D"
+                mess = "参加できるなら「#できる」、できないなら「#できない」と答えてね。"
                 line.api.push_message(
                     line_group_id, TextSendMessage(text=mess))
-        elif task_check_type == TaskCheckType.TommorowImportantTasksCheck:
-            # 明日が期限の重要タスクを通知(グループのみ)
-            group_task_map = {}
-            for task in Task.objects.filter(deadline__range=get_tommorow_range(), importance=TaskImportance.High.name, group__isnull=False):
-                group = task.group
-                if group.line_group.group_id in group_task_map:
-                    group_task_map[group.line_group.group_id].append(task)
-                else:
-                    group_task_map[group.line_group.group_id] = [task]
+            else:
+                mess = "こんにちは。明日が期限の重要なタスクは以下のとおりだよ。"
+                line.api.push_message(
+                    line_group_id, TextSendMessage(text=mess))
+                # タスク一覧を作成
+                mess = ""
+                for task_check_job in ordered_task_check_job_list:
+                    mess += "{}. {}(期限: {})\n".format(task_check_job.check_number, task_check_job.task.name,
+                                                      convert_deadline_to_string(task_check_job.task.deadline))
+                    mess += "メンバー：{}\n".format(
+                        "、".join([member.name for member in task_check_job.task.participants.all()]))
+                mess = mess.rstrip("\n")
+                line.api.push_message(
+                    line_group_id, TextSendMessage(text=mess))
+                mess = "これらのタスクに参加できるかできないか答えてね。"
+                line.api.push_message(
+                    line_group_id, TextSendMessage(text=mess))
+                mess = "例えば、1番のタスクに参加できて2番はできない場合は\n======\n#できる\n1\n======\n#できない\n2\n======\nのように答えてね。"
+                line.api.push_message(
+                    line_group_id, TextSendMessage(text=mess))
 
-            for line_group_id, task_list in group_task_map.items():
-                group = line.utilities.get_group_by_line_group_id_from_database(
-                    line_group_id)
-                # 確認タスクを追加
-                check_number_count = 1
-                for task in task_list:
-                    task_check = TaskJoinCheckJob.objects.filter(task=task)
-                    # すでに登録されていたら飛ばす
-                    if not task_check.exists():
-                        # 空いている確認番号を探す
-                        while TaskJoinCheckJob.objects.filter(check_number=check_number_count).exists():
-                            check_number_count += 1
-                        # タスク確認の登録(テストで期限を12時間後にする)
-                        TaskJoinCheckJob.objects.create(
-                            group=task.group, task=task, check_number=check_number_count, deadline=datetime.datetime.now() + datetime.timedelta(hours=12))
+            # タスク参加確認を開始
+            add_message_command_group(group, "タスク参加確認")
 
-                # 確認タスク一覧を作成
-                important_checking_tasks = TaskJoinCheckJob.objects.filter(
-                    group=group, task__importance=TaskImportance.High.name)
-                ordered_checking_task_list = [check_task for check_task in sorted(
-                    important_checking_tasks.all(), key=lambda check_task: check_task.check_number)]
-
-                # 通知
-                if len(ordered_checking_task_list) == 1:
-                    check_task = ordered_checking_task_list[0]
-                    task = check_task.task
-                    mess = "こんにちは。\n重要なタスク「{}」が明日の{}からあるよ。".format(
-                        task.name, convert_deadline_to_string(task.deadline))
-                    line.api.push_message(
-                        line_group_id, TextSendMessage(text=mess))
-                    mess = "メンバーの{}はこのタスクに参加できる？".format(
-                        "".join(["「{}」".format(member.name) for member in task.participants.all()]))
-                    line.api.push_message(
-                        line_group_id, TextSendMessage(text=mess))
-                    mess = "参加できるなら「#できる」、できないなら「#できない」と答えてね。"
-                    line.api.push_message(
-                        line_group_id, TextSendMessage(text=mess))
-                else:
-                    mess = "こんにちは。明日が期限の重要なタスクは以下のとおりだよ。"
-                    line.api.push_message(
-                        line_group_id, TextSendMessage(text=mess))
-                    # タスク一覧を作成
-                    mess = ""
-                    for check_task in ordered_checking_task_list:
-                        mess += "{}. {}(期限: {})\n".format(check_task.check_number, check_task.task.name,
-                                                          convert_deadline_to_string(check_task.task.deadline))
-                        mess += "メンバー：{}\n".format(
-                            "、".join([member.name for member in check_task.task.participants.all()]))
-                    mess = mess.rstrip("\n")
-                    line.api.push_message(
-                        line_group_id, TextSendMessage(text=mess))
-                    mess = "これらのタスクに参加できるかできないか答えてね。"
-                    line.api.push_message(
-                        line_group_id, TextSendMessage(text=mess))
-                    mess = "例えば、1番のタスクに参加できて2番はできない場合は\n======\n#できる\n1\n======\n#できない\n2\n======\nのように答えてね。"
-                    line.api.push_message(
-                        line_group_id, TextSendMessage(text=mess))
-
-                # タスク参加確認を開始
-                add_message_command_group(group, "タスク参加確認")
-        elif task_check_type == TaskCheckType.TasksPreRemindAndCheck:
-            pass
-        else:
-            assert False
+    @staticmethod
+    def handle_soon_tasks_remind_and_check():
+        '''もうすぐのタスクのリマインドとチェック'''
+        pass
