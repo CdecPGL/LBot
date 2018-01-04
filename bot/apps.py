4a2759a96495495be6c336939cb6363d179cb3d5
Cdec <cdecrement@gmail.com>
2018-01-04 23:40:14 +0900

    てすと

--- bot/apps.py
+++ bot/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
 
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
+def test_job():
+    print("testsetsetsets")
+
 class BotConfig(AppConfig):
     name = 'bot'
+
+    def ready(self):
+        '''アプリ起動時の処理'''
+        print("初期化。")
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(test_job, "interval", minutes=1)
+        scheduler.start()
+
