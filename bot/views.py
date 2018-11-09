9a9674efa0faed7595e226c23ee827afdf54ce18
Cdec <cdecrement@gmail.com>
2018-11-10 00:23:19 +0900

    [update] update views

--- bot/views.py
+++ bot/views.py
@@ -5,6 +5,9 @@ from .line import event_handlers as event_handlers
 
 
 def callback(request):
-    event_handlers.event_handler.handle(request.body.decode('utf-8'),
-                                        request.META["HTTP_" + "X_LINE_SIGNATURE"])  # 元はX-Line-Signature
+    line_signeture_key = "HTTP_" + "X_LINE_SIGNATURE"  # 元はX-Line-Signature
+    # LINEからのリクエストの場合
+    if line_signeture_key in request.META:
+        event_handlers.event_handler.handle(request.body.decode(
+            'utf-8'), request.META[line_signeture_key])
     return HttpResponse()
